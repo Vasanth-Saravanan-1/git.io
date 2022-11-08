65a4c87c2e55aa4813b594df7d8884a5ee27bb33
Vasanth Saravanan <vasanthsaravanancdm@gmail.com>
2022-11-08 08:01:59 +0530

    Updates

---
+++ precache-manifest.0ef8371ea10910d75a8442d7d378f01e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf8b110eb8483dd45598",
+    "url": "/github.io/static/css/main.6828a7ae.chunk.css"
+  },
+  {
+    "revision": "cf8b110eb8483dd45598",
+    "url": "/github.io/static/js/main.cf8b110e.chunk.js"
+  },
+  {
+    "revision": "68d85c9b0ba336868cc8",
+    "url": "/github.io/static/js/1.68d85c9b.chunk.js"
+  },
+  {
+    "revision": "627298a29158292ca362",
+    "url": "/github.io/static/js/2.627298a2.chunk.js"
+  },
+  {
+    "revision": "732f06db0d409a980c14",
+    "url": "/github.io/static/js/runtime~main.732f06db.js"
+  },
+  {
+    "revision": "35b0d62d16995a79e0bc100386c1c53f",
+    "url": "/github.io/index.html"
+  }
+];
